9f81f40d13c37f0b255341cccfc98f63408a08e8
JBlay10 <joseblay10@gmail.com>
2022-08-16 16:27:55 -0500

    Changed a class name, enabled buttons and added prompts and variables to my script

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,8 +1,11 @@
 // Assignment code here
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var chars = "";
+var numbers = "0123456789";
+var uppCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowCase = "abcdefghijklmnopqrstuvwxyz";
+var specialChar = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+const generateBtn = document.getElementById('generateBtn')
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -15,3 +18,10 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Prompt cards for button
+function generatePassword() {
+  var passwordLength = prompt("Enter the number of characters you want for your new password. Must be min. 8 & max. 128 characters long.");
+  var numbers = confirm("Do you want numbers in your password?");
+  var lowCase = confirm("Do you want lowercases in your password?")
+}
