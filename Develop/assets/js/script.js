1190932d57724a47debb2a391047a1b19231ff6f
JBlay10 <joseblay10@gmail.com>
2022-08-17 16:22:33 -0500

    Fixed issues in my script where it generated only 4 characters

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -28,88 +28,43 @@ function generatePassword() {
   var specialChars = confirm("Do you want special characters in your password?");
 
   // Min. and Max count for characters
-  var minCount = 4;
+  var minCount = 8;
   var maxCount = 128;
 
-  // No minimum or maximum for characters
-  var minLowCases = "";
-  var minUppCases = "";
-  var minSpecialChars = "";
-  var minNumbers = "";
-
-  var maxLowCases = "";
-  var maxUppCases = "";
-  var maxSpecialChars = "";
-  var maxNumbers = ""; 
-
+  if (numberss === false && lowCases === false && uppCases === false && specialChars === false){
+    alert("Bruuuh...");
+    var numberss = confirm("Do you want numbers in your password?");
+    var lowCases = confirm("Do you want lowercases in your password?");
+    var uppCases = confirm("Do you want uppercases in your password?");
+    var specialChars = confirm("Do you want special characters in your password?");
+  }
   //Generator
-  var functionArray = {
-    getNumbers: function() {
-      return numbers[Math.floor(Math.random() * numbers.length)];
-    },
-
-    getUppCase: function() {
-      return uppCase[Math.floor(Math.random() * uppCase.length)];
-    },
-
-    getLowCase: function() {
-      return lowCase[Math.floor(Math.random() * lowCase.length)];
-    },
-
-    getSpecialChar: function(){
-      return specialChar[Math.floor(Math.random() * specialChar.length)];
+  var functionArray = []
+    if (numberss) {
+      functionArray = functionArray.concat(numbers);
     }
-  };
+    
+    if (lowCases) {
+      functionArray = functionArray.concat(lowCase);
+    }
 
-  if (numberss === true) {
-    minNumbers = functionArray.getNumbers();
-    minCount++;
+    if (uppCases) {
+      functionArray = functionArray.concat(uppCase);
+    }
 
-   // maxNumbers = functionArray.getNumbers();
-   // maxCount--;
-  } //else {
-
- // }
-
-  if (uppCases === true) {
-    minUppCases = functionArray.getUppCase();
-    minCount++;
-
-   // maxUppCases = functionArray.getUppCase();
-    //maxCount--;
-  } //add else in future ask tutor
-
-  if (specialChars === true) {
-    minSpecialChars = functionArray.getSpecialChar();
-    minCount++;
-
-   // maxSpecialChars = functionArray.getSpecialChar();
-    //maxCount--;
-  }
-
-  if (lowCases === true) {
-    minLowCases = functionArray.getLowCase();
-    minCount++;
-
-    //maxLowCases = functionArray.getLowCase();
-    //maxCount--;
-  }
+    if (specialChars) {
+      functionArray = functionArray.concat(specialChar);
+    }
 
   //Displayed password
   var randomPasswGen = "";
+  //var newPassword = [];
+  for (let i = 0; i < passwLength ; i++) {
+    randomPasswGen = randomPasswGen + functionArray[Math.floor(Math.random() * functionArray.length)];
 
-  for (let i = 0; i < (parseInt(passwLength) - minCount); i++) {
-    var randomNumbPickd = Math.floor(Math.random() * 4);
-
-    randomPasswGen += randomNumbPickd;
+    //randomPasswGen += randomNumbPickd;
   }
-
-  randomPasswGen += minNumbers;
-  randomPasswGen += minUppCases;
-  randomPasswGen += minLowCases;
-  randomPasswGen += minSpecialChars;
 
   return randomPasswGen;
   
-
 }
