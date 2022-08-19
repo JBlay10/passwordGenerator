8d6d27325e19d5b88b371f92e256a4885191e624
JBlay10 <joseblay10@gmail.com>
2022-08-19 17:28:56 -0500

    Fixed the conditional statement for User input max 128 chars, min 8 chars. and NaN for passL.

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 var chars = "";
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var uppCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "~", "`", "|", "?", "<", ">", ".", "/", "-", "="];
@@ -27,30 +27,28 @@ function generatePassword() {
   var uppCases = confirm("Do you want uppercases in your password?");
   var specialChars = confirm("Do you want special characters in your password?");
 
-  // Min. and Max count for characters
-  var minCount = 8;
-  var maxCount = 128;
+  // If user inputs a 'letter' instead of a 'number', then prompt ends.
+  if (Number.isNaN(passwLength)) {
+    alert("Only numbers are allowed for password length.");
+    return null;
+  }
+  
+  // If user inputs number of characters below 8, then promt ends.
+  if (passwLength < 8) {
+    alert("Password must be at least 8 characters.");
+    return null;
+  }
 
-  // If else statement for when User inputs number of characters (This still needs some work).
-  if (passwLength >= minCount && passwLength <= maxCount) {
-    alert("Your password is " + passwLength + " characters long.");
-  } else{
-    alert("Your password is " + passwLength + " characters long, doesn't meet the requirements.");
-    var passwLength = prompt("Enter the number of characters you want for your new password. Must be min. 8 & max. 128 characters long.");
-    var numberss = confirm("Do you want numbers in your password?");
-    var lowCases = confirm("Do you want lowercases in your password?");
-    var uppCases = confirm("Do you want uppercases in your password?");
-    var specialChars = confirm("Do you want special characters in your password?");
-    //return;
+  // If user inputs number of characters above 128, then promt ends.
+  if (passwLength > 128) {
+    alert("Password must be under 128 characters.");
+    return null;
   }
 
   // If user doesn't select any of the options
   if (numberss === false && lowCases === false && uppCases === false && specialChars === false){
     alert("Bruuuh...");
-    var numberss = confirm("Do you want numbers in your password?");
-    var lowCases = confirm("Do you want lowercases in your password?");
-    var uppCases = confirm("Do you want uppercases in your password?");
-    var specialChars = confirm("Do you want special characters in your password?");
+    return null;
   }
   //Generator
   var functionArray = []
